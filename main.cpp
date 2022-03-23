fd361b120367b464b78aa58fe8c79e0c2d625be9
简尚全 <jianshangquan3@gmail.com>
2022-03-23 11:10:21 +0630

    string pointer

--- main.cpp
+++ main.cpp
@@ -106,46 +106,51 @@ int main(){
 
 
 
-    // References
+    // // References
     
-    int int_data{33};
-    double double_data{55};
+    // int int_data{33};
+    // double double_data{55};
 
 
-    int& ref_int_data{int_data};
-    double& ref_double_data{double_data};
+    // int& ref_int_data{int_data};
+    // double& ref_double_data{double_data};
 
 
-    std::cout << "int_data : " << int_data << std::endl;
-    std::cout << "&int_data : " << &int_data << std::endl;
-    std::cout << "double_data : " << double_data << std::endl;
-    std::cout << "&double_data : " << &double_data << std::endl;
+    // std::cout << "int_data : " << int_data << std::endl;
+    // std::cout << "&int_data : " << &int_data << std::endl;
+    // std::cout << "double_data : " << double_data << std::endl;
+    // std::cout << "&double_data : " << &double_data << std::endl;
 
-    std::cout << "==========================================" << std::endl;
+    // std::cout << "==========================================" << std::endl;
 
-    std::cout << "ref_int_data : " << ref_int_data << std::endl;
-    std::cout << "&ref_int_data : " << &ref_int_data << std::endl;
-    std::cout << "ref_double_data : " << ref_double_data << std::endl;
-    std::cout << "&ref_double_data : " << &ref_double_data << std::endl;
+    // std::cout << "ref_int_data : " << ref_int_data << std::endl;
+    // std::cout << "&ref_int_data : " << &ref_int_data << std::endl;
+    // std::cout << "ref_double_data : " << ref_double_data << std::endl;
+    // std::cout << "&ref_double_data : " << &ref_double_data << std::endl;
 
 
-    int_data = 111;
-    double_data = 67.2;
+    // int_data = 111;
+    // double_data = 67.2;
 
-    std::cout << std::endl << "After data changed " << std::endl;
+    // std::cout << std::endl << "After data changed " << std::endl;
 
-    std::cout << "int_data : " << int_data << std::endl;
-    std::cout << "&int_data : " << &int_data << std::endl;
-    std::cout << "double_data : " << double_data << std::endl;
-    std::cout << "&double_data : " << &double_data << std::endl;
+    // std::cout << "int_data : " << int_data << std::endl;
+    // std::cout << "&int_data : " << &int_data << std::endl;
+    // std::cout << "double_data : " << double_data << std::endl;
+    // std::cout << "&double_data : " << &double_data << std::endl;
 
-    std::cout << "==========================================" << std::endl;
+    // std::cout << "==========================================" << std::endl;
 
-    std::cout << "ref_int_data : " << ref_int_data << std::endl;
-    std::cout << "&ref_int_data : " << &ref_int_data << std::endl;
-    std::cout << "ref_double_data : " << ref_double_data << std::endl;
-    std::cout << "&ref_double_data : " << &ref_double_data << std::endl;
+    // std::cout << "ref_int_data : " << ref_int_data << std::endl;
+    // std::cout << "&ref_int_data : " << &ref_int_data << std::endl;
+    // std::cout << "ref_double_data : " << ref_double_data << std::endl;
+    // std::cout << "&ref_double_data : " << &ref_double_data << std::endl;
 
+
+
+    string string1 = "fdsafdsa";
+    string *p_string2 = &string1;
+    std::cout << string1 << " memory address " << p_string2 << " get value from address " << *p_string2;
 
     return 0;
 }
